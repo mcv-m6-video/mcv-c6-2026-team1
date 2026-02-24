085dd67225a7dd20e62d3111ac0acb103076174f
Diiego-H <diegohernandezanton@gmail.com>
2026-02-24 16:57:45 +0100

    Added extract_video util

--- Week1/src/video_utils.py
+++ Week1/src/video_utils.py
@@ -26,7 +26,7 @@ def load_gt_json():
         gts[ann["image_id"]].append(ann["bbox"])
     return gts
 
-def play_video(video_path=DEFAULT_VIDEO_PATH, show_gts=True, start_frame=535, width=640, height=360):
+def play_video(video_path=DEFAULT_VIDEO_PATH, show_gts=True, test_video=True, width=640, height=360):
     """
     Plays a video inside a Jupyter Notebook cell.
     """
@@ -42,11 +42,11 @@ def play_video(video_path=DEFAULT_VIDEO_PATH, show_gts=True, start_frame=535, wi
     frame_duration = 1.0 / fps
 
     # n_train=535
-    frame_idx = start_frame
+    frame_idx = 535 if test_video else 0
     try:
         while cap.isOpened():
             start_time = time.time()
-            
+
             ret, frame = cap.read()
             if not ret: 
                 print("End of video.")
@@ -76,3 +76,61 @@ def play_video(video_path=DEFAULT_VIDEO_PATH, show_gts=True, start_frame=535, wi
         print("\nStream stopped by user.")
     finally:
         cap.release()
+
+def extract_video(
+    video_path=DEFAULT_VIDEO_PATH, 
+    output_path="output.mp4", 
+    show_gts=True, 
+    test_video=True,
+    start_frame=0, 
+    end_frame=-1
+):
+    """
+    Extracts a video segment, saving it to an output path (optionally drawing GTs).
+    """
+    if show_gts:
+        gts = load_gt_json()
+        
+    cap = load_video(video_path)
+    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    size = ( int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)) )
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # Fast-forward to the start_frame efficiently
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+    
+    # Initialize OpenCV VideoWriter
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+    out = cv2.VideoWriter(output_path, fourcc, fps, size)
+
+    if end_frame < 0 or n_frames < end_frame:
+        end_frame = n_frames
+
+    print(f"Extracting frames {start_frame}-{end_frame} from {video_path}...")
+
+    # n_train=535
+    frame_idx = (535 if test_video else 0) + start_frame
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            cap.release()
+            out.release()
+            break
+
+        # Draw ground truths if requested and available for this frame
+        if show_gts:
+            for (x, y, w, h) in gts[frame_idx]:
+                x1, y1 = int(x), int(y)
+                x2, y2 = int(x + w), int(y + h)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        
+        # Write frame to the output file
+        out.write(frame)
+
+        frame_idx += 1
+
+    print(f"Frames successfully saved to '{output_path}'.")
+
+if __name__ == "__main__":
+    # Example Usage: Extract the test frames from the original video
+    extract_video(test_video=False, start_frame=535)
