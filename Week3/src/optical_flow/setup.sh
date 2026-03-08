9e473dd398e0f256e62687efa347560b4725d33d
urijus <orioljuan28@gmail.com>
2026-03-08 19:16:28 +0100

    Add setup.sh

--- Week3/src/optical_flow/setup.sh
+++ Week3/src/optical_flow/setup.sh
@@ -4,8 +4,8 @@ set -e
 echo "Initializing git submodules..."
 git submodule update --init --recursive
 
-PRETRAINED_DIR="./external/pretrained"
-mkdir -p "$PRETRAINED_DIR"
+EXTERNAL_DIR="./external"
+mkdir -p "$EXTERNAL_DIR"
 
 echo "Installing gdown..."
 pip install -q gdown
@@ -14,16 +14,16 @@ echo "Downloading pretrained models..."
 
 # Download GMFLow models
 FILE_ID=1d5C5cgHIxWGsFR1vYs5XrQbbUiZl9TX2
-ZIP_PATH="$PRETRAINED_DIR/pretrained.zip"
+ZIP_PATH="$EXTERNAL_DIR/pretrained.zip"
 gdown --id "$FILE_ID" -O "$ZIP_PATH"
-unzip -o "$ZIP_PATH" -d "$PRETRAINED_DIR"
+unzip -o "$ZIP_PATH" -d "$EXTERNAL_DIR"
 rm "$ZIP_PATH"
 
 # Download MemFLow models
-wget -O "$PRETRAINED_DIR/MemFlowNet_kitti.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_kitti.pth"
-wget -O "$PRETRAINED_DIR/MemFlowNet_T_kitti.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_T_kitti.pth"
-wget -O "$PRETRAINED_DIR/MemFlowNet_sintel.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_sintel.pth"
-wget -O "$PRETRAINED_DIR/MemFlowNet_T_sintel.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_T_sintel.pth"
+wget -O "$EXTERNAL_DIR/pretrained/MemFlowNet_kitti.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_kitti.pth"
+wget -O "$EXTERNAL_DIR/pretrained/MemFlowNet_T_kitti.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_T_kitti.pth"
+wget -O "$EXTERNAL_DIR/pretrained/MemFlowNet_sintel.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_sintel.pth"
+wget -O "$EXTERNAL_DIR/pretrained/MemFlowNet_T_sintel.pth" "https://github.com/DQiaole/MemFlow/releases/download/v1.0.0/MemFlowNet_T_sintel.pth"
 
 
 echo "Compiling PyFlow files..."
