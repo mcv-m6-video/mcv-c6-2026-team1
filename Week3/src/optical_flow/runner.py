ba364a0f883cb08dc0305ef928db199a73bc88a7
urijus <orioljuan28@gmail.com>
2026-03-08 18:26:57 +0100

    Add MemFlow, end of task 1.1

--- Week3/src/optical_flow/runner.py
+++ Week3/src/optical_flow/runner.py
@@ -6,14 +6,13 @@ import numpy as np
 from pathlib import Path
 
 from src.optical_flow.evaluation import load_gt, compute_msen, compute_pepn
-from src.optical_flow.pyflow_method import run_pyflow, load_images_pyflow
-from src.optical_flow.gmflow_method import run_gmflow, build_gmflow
 
 IMG_SEQ = 45
 IMG_PATH = Path("./data/optical_flow/training/image_0/")
 GT_PATH = Path("./data/optical_flow/training/flow_noc/")
 RESULTS_DIR = Path("./results")
 RESULTS_DIR.mkdir(exist_ok=True)
+PRETRAINED_MODELS_PATH = Path("./external/pretrained/")
 
 
 
@@ -41,21 +40,84 @@ def save_flow_visualization(flow, out_path):
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     cv2.imwrite(str(out_path), rgb)
 
+# Helper to parse MemFlow modes
+def _parse_memflow_method(method: str):
+    parts = method.lower().split("_")
+    if parts[0] != "memflow":
+        raise ValueError(f"Not a memflow method: {method}")
+
+    is_t = False
+    stage = "kitti"   # default
+
+    if len(parts) >= 2:
+        if parts[1] == "t":
+            is_t = True
+            if len(parts) >= 3:
+                stage = parts[2]
+        else:
+            stage = parts[1]
+
+    return is_t, stage
+
 def run_sequence(seq=IMG_SEQ, method="pyflow", method_params=None, img_path=IMG_PATH, gt_path=GT_PATH):
     img1_path, img2_path = return_image_paths(seq, img_path)
-
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
     if method == "pyflow":
+        from src.optical_flow.pyflow_method import run_pyflow, load_images_pyflow
         img1, img2 = load_images_pyflow(img1_path, img2_path)
         flow, info = run_pyflow(img1, img2, params=method_params)
     elif method == "gmflow":
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = build_gmflow(device=device)
+        from src.optical_flow.gmflow_method import run_gmflow, build_gmflow
+
+        model = build_gmflow(device=device) # Used default ckpt
         flow, info = run_gmflow(
             model, 
             img1_path, 
             img2_path, 
             inference_params=method_params,
             device=device)
+    elif method.startswith("memflow"):
+        from src.optical_flow.memflow_method import run_memflow, build_memflow
+        is_t, stage = _parse_memflow_method(method)
+
+        if not is_t:
+            if stage == "sintel":
+                from configs.sintel_memflownet import get_cfg
+            elif stage == "kitti":
+                from configs.kitti_memflownet import get_cfg
+            else:
+                raise NotImplementedError(f"Unsupported MemFlow stage: {stage}")
+            
+            ckpt = str(PRETRAINED_MODELS_PATH / "MemFlowNet_kitti.pth")
+            name = "MemFlowNet"
+
+        else:
+            if stage == "sintel":
+                from configs.sintel_memflownet_t import get_cfg
+            elif stage == "kitti":
+                from configs.kitti_memflownet_t import get_cfg
+            else:
+                raise NotImplementedError(f"Unsupported MemFlow-T stage: {stage}")
+
+            ckpt = str(PRETRAINED_MODELS_PATH / "MemFlowNet_T_kitti.pth")
+            name = "MemFlowNet_T"
+
+        cfg = get_cfg()
+        cfg.update({
+            "name": name,
+            "stage": stage,
+            "restore_ckpt": ckpt,
+        })
+        model = build_memflow(cfg, cfg.restore_ckpt, device=device)
+        flow, info = run_memflow(
+            model, 
+            cfg,
+            img1_path, 
+            img2_path, 
+            runtime_config=method_params,
+            device=device)
+
     else:
         raise ValueError(f"Unknown method: {method}")
 
