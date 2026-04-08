250e93d7e00fc1f3ef2d7df17057d643ba7d6678
Diiego-H <diegohernandezanton@gmail.com>
2026-04-08 09:07:28 +0200

    Updated best model

--- project2/Task1/get_best_weights.sh
+++ project2/Task1/get_best_weights.sh
@@ -4,4 +4,4 @@ SAVE_DIR=./../SoccerNet/SN-BAS-2025-SAVE_DIR
 mkdir -p ${SAVE_DIR}/best
 
 # Download directly to the destination
-gdown 1PKIUbJq1g7qF2XThT6AzHcjCrEtijbka -O ${SAVE_DIR}/best/checkpoint_best.pt
+gdown 1-I4XEijAlISe8Tz_fqM0UK9LVDskcMG3 -O ${SAVE_DIR}/best/checkpoint_best.pt
